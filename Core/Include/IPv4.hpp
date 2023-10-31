5be4e4c213b6531a11e5838d2769e710064996ef
Tate House <tatehouse100@gmail.com>
2023-10-31 11:36:14 -0400

    Feature: Parse IPv4 string into binary

---
+++ Core/Include/IPv4.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <bitset>
+#include <string>
+
+namespace Core {
+class IPv4 final {
+public:
+    explicit IPv4(const std::string& address);
+
+public:
+    [[nodiscard]] const std::bitset<32>& getAddress() const;
+
+private:
+    static const std::bitset<32> parse(const std::string& address);
+
+private:
+    const std::bitset<32> address;
+};
+}
---
+++ Test/Source/IPv4Test.cpp
@@ -0,0 +1,36 @@
+#include <gmock/gmock.h>
+
+#include "IPv4.hpp"
+
+namespace Core::Test {
+class IPv4Test : public ::testing::Test {
+public:
+    IPv4Test() = default;
+    ~IPv4Test() override = default;
+
+protected:
+    IPv4 ip {"152.20.192.0"};
+};
+
+TEST_F(IPv4Test, GivenValidIPv4String_WhenConstruct_ThenConvertsStringToBinary) {
+    const std::bitset<32> expected {0b10011000'00010100'11000000'00000000};
+
+    EXPECT_THAT(ip.getAddress(), testing::Eq(expected));
+}
+
+TEST_F(IPv4Test, GivenIPv4StringWithTooFewOctetSeparators_WhenConstruct_ThenThrowsInvalidArgument) {
+    EXPECT_THROW(IPv4 {"152.20.192"}, std::invalid_argument);
+}
+
+TEST_F(IPv4Test, GivenIPv4StringWithTooManyOctets_WhenConstruct_ThenThrowsInvalidArgument) {
+    EXPECT_THROW(IPv4 {"152.20.192.20.0"}, std::invalid_argument);
+}
+
+TEST_F(IPv4Test, GivenIPv4StringWithOctetWithTooManyDigits_WhenConstruct_ThenThrowsInvalidArgument) {
+    EXPECT_THROW(IPv4 {"152.20.192.0000"}, std::invalid_argument);
+}
+
+TEST_F(IPv4Test, GivenIPv4StringWithOctetWithTooFewDigits_WhenConstruct_ThenThrowsInvalidArgument) {
+    EXPECT_THROW(IPv4 {"152.20..192"}, std::invalid_argument);
+}
+}
